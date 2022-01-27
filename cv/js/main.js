610db840b8e5b55769a39e90f887e273325148c9
RasmusJuelN <rasmusn686@hotmail.com>
2022-01-27 14:42:37 +0100

    cursor

--- cv/js/main.js
+++ cv/js/main.js
@@ -1,17 +1,49 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+{
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", mobileMenu);
+    hamburger.addEventListener("click", mobileMenu);
 
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+    function mobileMenu() {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    }
+    const navLink = document.querySelectorAll(".nav-link");
+
+    navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+    function closeMenu() {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }
 }
-const navLink = document.querySelectorAll(".nav-link");
 
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+{
+    $(window).ready(function(){
 
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+        let mouseX = 0;
+        let mouseY = 0;
+        let xp = 0;
+        let yp = 0;
+      
+        $(document).mousemove(function(e){
+          $(".custom__cursor__inner").css({
+            transform: 'translate(' + (e.clientX - 3.20) + 'px, ' + (e.clientY - 3.25) + 'px)'
+          });
+    
+          mouseX = e.clientX - 10;
+          mouseY = e.clientY - 10;
+        });
+      
+        setInterval(function(){
+          xp += ((mouseX - xp)/5);
+          yp += ((mouseY - yp)/5);
+          $(".custom__cursor__outer").css({transform: 'translateX('+ (xp - 9) +'px) translateY('+ (yp - 9) +'px)'} );
+        }, 10);
+    })
+
+
+
+
+
 }
